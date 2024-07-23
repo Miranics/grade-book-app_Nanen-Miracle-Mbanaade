6f7b67957a3cd3d97788d430d1a66ddf1e90d3f1
Miranics <002nasya@gmail.com>
2024-07-23 05:11:34 -0700

    course file fixed

---
+++ course/course.py
@@ -0,0 +1,8 @@
+# This file defines the Course class, representing courses in the GradeBook application.
+
+class Course:
+    def __init__(self, name, trimester, credits):
+        self.name = name
+        self.trimester = trimester
+        self.credits = credits
+

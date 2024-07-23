99bd2ff5897b9e7a5ce0499c8a4c8d712be287f0
miranics <002nasya@gmail.com>
2024-07-23 12:12:05 +0200

    Defining course class

---
+++ course.py
@@ -0,0 +1,7 @@
+# This file defines the Course class, representing courses in the GradeBook application.
+
+class Course:
+    def __init__(self, name, trimester, credits):
+        self.name = name
+        self.trimester = trimester
+        self.credits = credits
